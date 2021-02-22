53321c792068e6f127d1a9f1ae9cc07db2e75902
Mason Kleinsorge <kleinsorgem97@gmail.com>
2021-02-21 19:50:30 -0600

    Implemented CMV

---
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  Quizzler-iOS13
+//
+//  Created by Angela Yu on 12/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var questionLable: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    var quizBrain = QuizBrain()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+    }
+
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
+        //check for correct answer
+        if quizBrain.checkAnswer(sender.currentTitle!) {
+            sender.backgroundColor = UIColor.green //answer is correct
+        } else {
+            sender.backgroundColor = UIColor.red //answer is wrong
+        }
+        
+        quizBrain.nextQuestion()
+        
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+    }
+    
+    @objc func updateUI(){
+        questionLable.text = quizBrain.getQuestionText()
+        progressBar.progress = quizBrain.getProgress()
+        
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
+    }
+    
+}
